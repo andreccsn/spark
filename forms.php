d1dfa15bbe9df90596c87eb4fa7ce20901e8b5db
André Cândido <andrecandido@MacBook-Pro-de-Andre.local>
2017-01-12 20:37:49 -0200

    Component: Tables

---
+++ forms.php
@@ -0,0 +1,57 @@
+<?php 
+    $pageTitle = 'Forms';
+    $heroTitle = 'Components';
+    $pageDescription = 'Diversos componentes reutilizáveis para você utilizar no desenvolvimento de intefaces administrativas.';
+    require 'header.php';
+?>
+
+<h2 class="page-title"><?= $pageTitle ?></h2>
+<p></p>
+
+<form>
+    <div class="form-group">
+        <label for="name">Nome</label>
+        <input type="text" name="name" id="name" class="form-field" placeholder="Digite seu nome completo">
+    </div>
+    <div class="form-group">
+        <label for="email">E-mail</label>
+        <input type="text" name="email" id="email" class="form-field" placeholder="Seu endereço de e-mail">
+    </div>
+    <div class="form-group">
+        <label for="password">Senha</label>
+        <input type="text" name="password" id="password" class="form-field" placeholder="Entre 8-12 caracteres">
+    </div>
+    <div class="form-group">
+        <label for="select-example">Exemplo de select</label>
+        <select id="select-example" class="form-field">
+            <option value="">-- Selecione</option>
+            <option value="">Valor 1</option>
+            <option value="">Valor 2</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <label for="textarea-example">Exemplo textarea</label>
+        <textarea id="textarea-example" class="form-field" rows="4"></textarea>
+    </div>
+    <legend>Radio Buttons</legend>
+    <div class="form-group check">
+        <label>
+            <input type="radio" name="radioExample1" value="1"> A primeira opção é esta
+        </label>
+        <label>
+            <input type="radio" name="radioExample1" value="2"> Está e a segunda opção, quando selecionada desmarca a primeira
+        </label>
+        <label>
+            <input type="radio" name="radioExample1" value="disabled" disabled> Esta opção está desativada
+        </label>
+    </div>
+    <div class="form-group check">
+        <label>
+            <input type="checkbox" name="checkboxExample1" value="On"> Li e aceitos os termos mencionados
+        </label>
+    </div>
+</form>
+
+
+
+<? require 'footer.php' ?>
---
+++ tables.php
@@ -0,0 +1,209 @@
+<?php 
+    $pageTitle = 'Tabelas';
+    $heroTitle = 'Components';
+    $pageDescription = 'Diversos componentes reutilizáveis para você utilizar no desenvolvimento de intefaces administrativas.';
+    require 'header.php';
+?>
+
+<h2 class="page-title"><?= $pageTitle ?></h2>
+<p></p>
+
+<h2>Basic Tables</h2>
+
+<table class="table">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>E-mail</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+    </tbody>
+</table>
+
+<h2>Bordered Tables</h2>
+
+<table class="table bordered">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>E-mail</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+    </tbody>
+</table>
+
+<h2>Striped Tables</h2>
+
+<table class="table striped">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>E-mail</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+    </tbody>
+</table>
+
+<h2>Hover Tables</h2>
+
+<table class="table hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>E-mail</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+    </tbody>
+</table>
+
+<h2>Small Tables</h2>
+
+<table class="table small">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>E-mail</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+    </tbody>
+</table>
+
+<h2>All Styles</h2>
+
+<table class="table bordered striped hover small">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>E-mail</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>André Cândido</td>
+            <td>Nogueira</td>
+            <td>candidocarvalho.andre@gmail.com</td>            
+        </tr>
+    </tbody>
+</table>
+
+<? require 'footer.php' ?>
